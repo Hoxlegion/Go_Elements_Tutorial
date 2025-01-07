dda74248564cee6ac698a11548a68d12e4aff4b0
Hoxlegion <hoxlegion@gmail.com>
2025-01-07 21:02:28 +0100

    refactor and added sum func

--- arrayslices/sum_test.go
+++ arrayslices/sum_test.go
@@ -4,13 +4,28 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-    numbers := [...]int{1, 2, 3, 4, 5}
+    t.Run("collection of 5 numbers", func(t *testing.T) {
+        numbers := []int{1, 2, 3, 4, 5}
 
-    got := Sum(numbers)
-    want := 15
 
-    if got != want {
-        t.Errorf("got %d want %d given, %v", got, want, numbers)
-    }
+        got := Sum(numbers)
+        want := 15
 
+        log(t, got, want, numbers)
+    })
+
+    t.Run("collection of any size", func(t *testing.T) {
+        numbers := []int{1, 2, 3}
+
+        got := Sum(numbers)
+        want := 6
+
+        log(t, got, want, numbers)
+    })
+}
+
+func log(t *testing.T, got, want int, numbers []int) {
+        if got != want {
+            t.Errorf("got %d want %d given, %v", got, want, numbers)
+        }
 }
