392942a7204ffd3a27d8095d2f489d092e87a7eb
Hoxlegion <hoxlegion@gmail.com>
2025-01-08 20:07:31 +0100

    Sums, SumTails

--- arrayslices/sum_test.go
+++ arrayslices/sum_test.go
@@ -7,23 +7,30 @@ import (
 
 func TestSum(t *testing.T) {
 
+    checkSum := func(t testing.TB, result, want int, numbers []int) {
+        t.Helper()
+
+        if result != want {
+            t.Errorf("got %d want %d given, %v", want, result, numbers)
+        }
+    }
     t.Run("collection of 5 numbers", func(t *testing.T) {
         numbers := []int{1, 2, 3, 4, 5}
 
 
-        got := Sum(numbers)
+        result := Sum(numbers)
         want := 15
 
-        log(t, got, want, numbers)
+        checkSum(t, result, want, numbers)
     })
 
     t.Run("collection of any size", func(t *testing.T) {
         numbers := []int{1, 2, 3}
 
-        got := Sum(numbers)
+        result := Sum(numbers)
         want := 6
 
-        log(t, got, want, numbers)
+        checkSum(t, result, want, numbers)
     })
 }
 
@@ -35,11 +42,29 @@ func TestSumAll(t *testing.T) {
         t.Errorf("got %d want %d", result, want)
 
     }
-
 }
 
-func log(t *testing.T, got, want int, numbers []int) {
-        if got != want {
-            t.Errorf("got %d want %d given, %v", got, want, numbers)
+func TestSumAllTails(t *testing.T) {
+
+    checkSums := func(t testing.TB, result, want []int) {
+        t.Helper()
+
+        if !slices.Equal[[]int](result, want) {
+            t.Errorf("got %v want %v", result, want)
         }
+    }
+    t.Run("make the sums of some slices", func(t *testing.T) {
+        result := SumAllTails([]int{1, 2}, []int{0, 9})
+        want := []int{2, 9}
+
+        checkSums(t, result, want)
+    })
+
+    t.Run("safely sum empty slices", func(t *testing.T) {
+        result := SumAllTails([]int{}, []int{0, 9})
+        want := []int{0, 9}
+
+        checkSums(t, result, want)
+    })
 }
+
