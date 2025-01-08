6e088f79f73b68293830af07839428cbe063ba2c
Hoxlegion <hoxlegion@gmail.com>
2025-01-08 21:08:33 +0100

    Area

--- structsMethodsInterfaces/perimeter_test.go
+++ structsMethodsInterfaces/perimeter_test.go
@@ -2,11 +2,27 @@ package main
 
 import "testing"
 
+type Rectangle struct {
+    Width float64
+    Height float64
+}
+
 func TestPerimeter(t *testing.T) {
-    result := Perimeter(10.0, 10.0)
+    rectangle := Rectangle{10.0, 10.0}
+    result := Perimeter(rectangle)
     want := 40.0
 
     if result != want {
         t.Errorf("got %.2f want %.2f", result, want)
+    }
+}
+
+func TestArea(t *testing.T) {
+    rectangle := Rectangle{12.0, 6.0}
+    result := Area(rectangle)
+    want := 72.0
+
+    if result != want {
+        t.Errorf("got %v want %v", result, want)
     }
 }
