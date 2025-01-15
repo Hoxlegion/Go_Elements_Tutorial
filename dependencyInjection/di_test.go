5070e9f98365a5d8cf7dceaf46d4ee38abc2d039
Hoxlegion <hoxlegion@gmail.com>
2025-01-15 20:20:40 +0100

    final di

--- dependencyInjection/di_test.go
+++ dependencyInjection/di_test.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"testing"
 )
 
-func GreetTest(t *testing.T) {
-    fmt.Println("Hello")
+func TestGreet(t *testing.T) {
+    buffer := bytes.Buffer{}
+    Greet(&buffer, "Chris")
+
+    got := buffer.String()
+    want := "Hello, Chris"
+
+    if got != want {
+        t.Errorf("got %q want %q", got, want)
+    }
 }
