c7d40a1b6dc7438f62d6a2ba8c81b773cdd39ab1
Hoxlegion <hoxlegion@gmail.com>
2025-01-20 19:07:54 +0100

    finished

--- mocking/mock.go
+++ mocking/mock.go
@@ -3,31 +3,39 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
 
 const (
-    finalWord = "Go!"
-    countdownStart = 3
+	finalWord      = "Go!"
+	countdownStart = 3
 )
 
 func CountDown(out io.Writer, sleeper Sleeper) {
-    for i := countdownStart; i > 0; i-- {
-        sleeper.Sleep()
-    }
+	for i := range countDownFrom(3) {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
 
-    for i := countdownStart; i > 0; i-- {
-        fmt.Fprintln(out, i)
-    }
+	fmt.Fprint(out, finalWord)
+}
 
-    fmt.Fprint(out, finalWord)
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+
+		}
+	}
 }
 
 func main() {
-    sleeper := &ConfigureableSleeper{1 * time.Second, time.Sleep}
-    CountDown(os.Stdout, sleeper)
+	sleeper := &ConfigureableSleeper{1 * time.Second, time.Sleep}
+	CountDown(os.Stdout, sleeper)
 }
-
 
 
