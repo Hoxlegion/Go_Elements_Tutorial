8cf0889e74b01c2d3b9cc30bb25869db8ac3ad81
Hoxlegion <hoxlegion@gmail.com>
2025-01-21 21:07:27 +0100

    tests for walking reflection(recursion)

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,3 +1,62 @@
 package main
 
+import (
+	"reflect"
+	"testing"
+)
 
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+		        Name string
+            }{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+                Name string
+                City string
+            }{"Chris", "Londen"},
+			[]string{"Chris", "Londen"},
+		},
+		{
+			"struct with non string field",
+			Person {
+                "Chris",
+                Profile{33, "Londen"},
+            },
+			[]string{"Chris", "Londen"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
