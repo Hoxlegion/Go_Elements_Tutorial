83ae1681559115bfac9ddbb9cb35caba0ff86abf
Hoxlegion <hoxlegion@gmail.com>
2025-01-23 19:43:08 +0100

    test and cancell

--- context/models.go
+++ context/models.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"context"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-	"time"
-)
-
 type Store interface {
 	Fetch() string
 	Cancel()
@@ -23,31 +15,6 @@ func (s *SpyStore) Fetch() string {
 }
 
 func (s *SpyStore) Cancel() {
-	s.cancelled = true
+    s.cancelled = true
 }
 
-func TestServer(t *testing.T) {
-	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		data := "hello, world"
-        store := &SpyStore{response: data}
-		svr := Server(store)
-
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-
-        cancellingCtx, cancel := context.WithCancel(request.Context())
-        time.AfterFunc(5*time.Millisecond, cancel)
-        request = request.WithContext(cancellingCtx)
-
-		response := httptest.NewRecorder()
-
-		svr.ServeHTTP(response, request)
-
-        if !store.cancelled {
-            t.Errorf("store was not told to cancel")
-        }
-
-		if response.Body.String() != data {
-			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-		}
-	})
-}
